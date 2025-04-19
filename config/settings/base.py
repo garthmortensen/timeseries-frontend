e0306f18896d9292d122eee674c4343f93e83c36
Garth Mortensen <mortensengarth@hotmail.com>
2025-04-18 20:04:42 -0500

    Netsec: Remove one middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,7 +43,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'djangosecure.middleware.SecurityMiddleware',
     'csp.middleware.CSPMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
